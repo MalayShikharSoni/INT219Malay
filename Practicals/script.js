3f2764fde687fe201aa3cc624ac25db61128dbb6
MalayShikharSoni <malayshikhar2004@gmail.com>
2025-02-22 19:24:33 +0530

    feb21

--- Practicals/script.js
+++ Practicals/script.js
@@ -1,79 +1,68 @@
+const boxes = document.querySelectorAll(".box");
 
-const box = document.querySelector(".box");
-const circle = document.querySelector(".hover-transition");
-const circleRadius = 20;
+boxes.forEach((box) => {
+    const circle = document.createElement("div");
+    circle.classList.add("hover-transition");
+    circle.style.backgroundColor = "aqua";
+    circle.style.borderRadius = "50%";
+    circle.style.width = "0px";
+    circle.style.height = "0px";
+    circle.style.position = "absolute";
+    circle.style.transform = "translate(-50%, -50%)";
+    box.appendChild(circle);
 
-box.addEventListener("mousemove", (e) => {
-    const boxRect = box.getBoundingClientRect();
-    let x = e.clientX - boxRect.left;
-    let y = e.clientY - boxRect.top;
+    box.addEventListener("mousemove", (e) => {
+        const boxRect = box.getBoundingClientRect();
+        let x = e.clientX - boxRect.left;
+        let y = e.clientY - boxRect.top;
 
-    // Constrain within box
-    x = Math.max(circleRadius, Math.min(x, boxRect.width - circleRadius));
-    y = Math.max(circleRadius, Math.min(y, boxRect.height - circleRadius));
+        circle.style.left = `${x}px`;
+        circle.style.top = `${y}px`;
+    });
 
-    // circle.style.transform = `translate(${x - circleRadius}px, ${y - circleRadius}px)`
+    box.addEventListener("mouseenter", () => {
+        gsap.to(circle, {
+            duration: 0.7,
+            width: '2000px',
+            height: '2000px',
+        });
 
-    circle.style.left = `${x}px`;
-    circle.style.top = `${y}px`;
+        gsap.to(box, {
+            scale: 1.1,
+            duration: 0.5,
+        });
 
-    // gsap.to(circle, {
-        
-    //     duration: 1,
-    //     width: '200px',
-    //     height: '200px',
-    // });
+        gsap.to(box.querySelector(".prev-content"), {
+            opacity: 0,
+            duration: 0.4,
+        });
+
+        gsap.to(box.querySelector(".next-content"), {
+            opacity: 1,
+            duration: 0.5,
+        });
+    });
+
+    box.addEventListener("mouseleave", () => {
+        gsap.to(circle, {
+            duration: 0.5,
+            width: '0px',
+            height: '0px',
+        });
+
+        gsap.to(box, {
+            scale: 1,
+            duration: 1,
+        });
+
+        gsap.to(box.querySelector(".prev-content"), {
+            opacity: 1,
+            duration: 1,
+        });
+
+        gsap.to(box.querySelector(".next-content"), {
+            opacity: 0,
+            duration: 0.4,
+        });
+    });
 });
-
-box.addEventListener("mouseenter", (e) => {
-    gsap.to(circle, {
-        duration: 0.7,
-        width: '2000px',
-        height: '2000px',
-    })
-
-    gsap.to(box, {
-        scale: 1.1,
-        duration: 0.5,
-
-    })
-
-    gsap.to(".prev-content", {
-        opacity: 0,
-        duration: 0.4,
-    })
-
-    gsap.to(".next-content", {
-        opacity: 1,
-        duration: 0.5,
-        
-    })
-
-
-})
-
-box.addEventListener("mouseleave", (e) => {
-    gsap.to(circle, {
-        duration: 0.5,
-        width: '0px',
-        height: '0px',
-    })
-    gsap.to(box, {
-        scale: 1,
-        duration: 1,
-    
-    })
-
-    gsap.to(".prev-content", {
-        opacity: 1,
-        duration: 1,
-    })
-
-    gsap.to(".next-content", {
-        opacity: 0,
-        duration: 0.4,
-        
-    })
-})
-
-
