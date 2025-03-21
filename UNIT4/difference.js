c9625294e5b311e599327a1a4b20232aaf943c72
MalayShikharSoni <malayshikhar2004@gmail.com>
2025-03-21 15:10:53 +0530

    mar21

--- UNIT4/difference.js
+++ UNIT4/difference.js
@@ -65,3 +65,24 @@ console.log(e >= f);
 
 console.log(5>>1);
 console.log(5<<1);
+
+//Ternary Operator
+console.log("Ternary Operator");
+let age = 20;
+let status = (age>=18) ? "Adult" : "Minor";
+console.log(status);
+console.log("\n");
+
+//Type Operators
+console.log("Type Operators");
+console.log("typeof 42", typeof 42);
+console.log("typeof '42'", typeof '42');
+console.log("typeof true", typeof true);
+console.log("typeof null", typeof null);
+console.log("typeof {}", typeof {});
+console.log("typeof []", typeof []);
+
+//instanceof example
+console.log("instanceof example");
+let arr = [1,2,3];
+console.log("arr instaneof Array ", arr instanceof Array);
